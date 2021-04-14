77fc017d2f5b7ff056f08d82b5f87453fab2b200
Djack#0009 <69060373+Djack-V@users.noreply.github.com>
2021-04-14 22:24:51 +0200

    MAJ 1.3
    add sql

---
+++ PopsDiner/sql.sql
@@ -0,0 +1,40 @@
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_popsdinerjob', 'popsdiner', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_popsdinerjob', 'popsdiner', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_popsdinerjob', 'popsdiner', 1)
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('popsdiner', 0, 'recruit', 'Recrue', 20, '{}'),
+	('popsdiner', 1, 'serveur', 'Serveur',40, '{}'),
+	('popsdiner', 2, 'coboss', 'Co-Gérant',60, '{}'),
+	('popsdiner', 3, 'boss', 'Patron', 80, '{}')
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('popsdiner', 'popsdiner')
+;
+
+INSERT INTO `items` (name, label) VALUES
+	('painburger', 'Pain à burger'),
+	('tomate', 'Tomate'),
+	('mozzarella', 'Mozzarella'),
+	('steak', 'Steak'),
+	('oignon', 'Oignon'),
+	('salade', 'Salade'),
+	('icetea', 'Ice-Tea'),
+	('water', 'Eau'),
+	('limonade', 'Limonade'),
+	('tomatecouper', 'Tranches de tomates'),
+	('mozzarellacouper', 'Tranches de mozzarella'),
+	('saladecouper', 'Pignon de salade'),
+	('steakcuit', 'Steak cuit'),
+	('oignonfrit', 'Oignon frit'),
+	('burger', 'Burger')
+;
